d75aa18ae0398cb281a4211d87af2133dccfe05d
Ebrar <16155052@mersin.edu.tr>
2023-02-04 13:13:50 +0300

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+ <div className='flex justify-between'>
+ <h2>Welcome the Dashboard</h2>
+ <h2>Hi again,Mea</h2>
+ </div>
+  )
+}
+
+export default Header
