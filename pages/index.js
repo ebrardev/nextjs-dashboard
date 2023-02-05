34fbe6c3aa54a963ec79f25969ef75dbe54746f3
Ebrar <16155052@mersin.edu.tr>
2023-02-05 13:56:22 +0300

    customer page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
+import BarChart from '@/components/BarChart'
 import Header from '@/components/Header'
+import RecentOrders from '@/components/RecentOrders'
+import TopCards from '@/components/TopCards'
 import Head from 'next/head'
 import Image from 'next/image'
 
@@ -14,7 +17,13 @@ export default function Home() {
       </Head>
       <main className=" bg-gray-100 min-h-screen">
         <Header/>
+        <TopCards/>
+        <div className='p-4 grid md:grid-cols-3 grid-cols-1 gap-4 '>
+          <BarChart/>
+          <RecentOrders/>
+        </div>
       
+    
       </main>
     </>
   )
